54958c08f64fca804516a7fb693e5f7202491f11
elias <elias.mamani@upeu.edu.pe>
2020-11-19 22:00:31 -0500

    plan2

---
+++ src/main/java/pe/edu/upeu/sysrubricas/daoImp/PlanDaoImpl.java
@@ -0,0 +1,40 @@
+package pe.edu.upeu.sysrubricas.daoImp;
+
+import java.util.Map;
+
+import pe.edu.upeu.sysrubricas.dao.PlanDao;
+import pe.edu.upeu.sysrubricas.entity.Plan;
+
+public class PlanDaoImpl implements PlanDao{
+
+	@Override
+	public int create(Plan p) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(Plan p) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(int id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Map<String, Object> read(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, Object> readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/pe/edu/upeu/sysrubricas/serviceimpl/PlanServiceImpl.java
@@ -0,0 +1,40 @@
+package pe.edu.upeu.sysrubricas.serviceimpl;
+
+import java.util.Map;
+
+import pe.edu.upeu.sysrubricas.entity.Plan;
+import pe.edu.upeu.sysrubricas.service.PlanService;
+
+public class PlanServiceImpl implements PlanService{
+
+	@Override
+	public int create(Plan p) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(Plan p) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(int id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Map<String, Object> read(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, Object> readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
